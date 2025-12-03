32ffdc986a34877f308b68e69c03ca10eca5a6dd
Phạm Hưng <hungphamhhh0408@gmail.com>
2025-12-03 15:46:25 +0700

    Add files via upload

---
+++ Ultrasonic_sensor_Hello.ino
@@ -0,0 +1,39 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3  , POSITIVE);
+
+
+const int trigPin = 6; 
+const int echoPin = 7; 
+float distance_cm;
+
+void setup() {
+  lcd.begin(16, 2); 
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);  
+}
+void loop() {
+  
+  // Generate microsecond pulses to initiate the ultrasonic sensor
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Measure the duration of the echo pulse on the echo pin
+  long duration = pulseIn(echoPin, HIGH);
+  
+  distance_cm = duration / 58.0;
+
+  // Display the distance on the LCD display
+  lcd.setCursor(6, 0); 
+  lcd.print("     "); 
+  lcd.setCursor(6, 0); 
+  if (distance_cm < 10) {
+     lcd.print("Hello");
+  } 
+  
+  delay(1000); // Delay for stability
+}
